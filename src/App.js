dfb543327bb5f15e2c6f2e5d5a3d2f1f23058781
Naga Manjula, Koutha <cnagamanju@gmail.com>
2018-11-30 15:18:04 -0800

    firebase databse setup done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import uuid from 'uuid';
+import firebase from 'firebase';
+
+var config = {
+   apiKey: "AIzaSyB8uATZRbcUpVKPAylQ7H3NA_XTRr70X0c",
+   authDomain: "reactsimplesurvey-489ef.firebaseapp.com",
+   databaseURL: "https://reactsimplesurvey-489ef.firebaseio.com",
+   projectId: "reactsimplesurvey-489ef",
+   storageBucket: "reactsimplesurvey-489ef.appspot.com",
+   messagingSenderId: "920712650132"
+ };
+ firebase.initializeApp(config);
+
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      id :'',
+      id :uuid.v1(),
       name:'',
       answers:{
         q1:'',
@@ -17,6 +30,8 @@ class App extends Component {
       },
       submitted:false
     }
+    this.handleQuestionChange = this.handleQuestionChange.bind(this);
+
   }
   handleSubmit(e){
     e.preventDefault();
@@ -29,15 +44,33 @@ class App extends Component {
   }
   handleSurveySubmit(e){
     e.preventDefault();
+    firebase.database().ref('surveys/'+this.state.id).set({
+      name:this.state.name,
+      answers:this.state.answers
+    });
     this.setState({
-      answers:{
-        q1:'A',
-        q2:'A',
-        q3:'A',
-        q4:'A',
-        q5:'A'
-      },
       submitted:true
+    },function(){
+      console.log(this.state);
+    });
+
+  }
+  handleQuestionChange(e){
+    e.preventDefault();
+    var answers = this.state.answers;
+    if(e.target.name === 'q1'){
+        answers.q1 = e.target.value;
+    }else if(e.target.name === 'q2'){
+        answers.q2 = e.target.value;
+    }else if(e.target.name === 'q3'){
+        answers.q3 = e.target.value;
+    }else if(e.target.name === 'q4'){
+        answers.q4 = e.target.value;
+    }else if(e.target.name === 'q5'){
+        answers.q5 = e.target.value;
+    }
+    this.setState({answers:answers},function(){
+      console.log(this.state);
     });
   }
   render() {
@@ -49,39 +82,39 @@ class App extends Component {
         <h3 ><center> Survey Questions</center></h3>
         <form onSubmit={this.handleSurveySubmit.bind(this)}>
         <label>1.Overall, how does our website meet your needs</label><br/>
-        <input type="radio" name ="q1" value ="extremelyWell"/>Extremely well<br/>
-        <input type="radio" name ="q1" value ="veryWell"/>Very well<br/>
-        <input type="radio" name ="q1" value ="somewhatWell"/>Somewhat well<br/>
-        <input type="radio" name ="q1" value ="notSoWell"/>Not so well<br/>
-        <input type="radio" name ="q1" value ="notAtAllWell"/>Not at all well<br/><br/>
+        <input type="radio" name ="q1" value ="extremelyWell" onChange ={this.handleQuestionChange}/>Extremely well<br/>
+        <input type="radio" name ="q1" value ="veryWell" onChange ={this.handleQuestionChange}/>Very well<br/>
+        <input type="radio" name ="q1" value ="somewhatWell" onChange ={this.handleQuestionChange}/>Somewhat well<br/>
+        <input type="radio" name ="q1" value ="notSoWell" onChange ={this.handleQuestionChange}/>Not so well<br/>
+        <input type="radio" name ="q1" value ="notAtAllWell" onChange ={this.handleQuestionChange}/>Not at all well<br/><br/>
 
         <label>2.How easy was it to find what you were  looking for on our website?</label><br/>
-        <input type="radio" name ="q2" value ="extremelyEasy"/>Extremely easy<br/>
-        <input type="radio" name ="q2" value ="veryEasy"/>Very easy<br/>
-        <input type="radio" name ="q2" value ="somewhatEasy"/>Somewhat easy<br/>
-        <input type="radio" name ="q2" value ="notSoEasy"/>Not so easy<br/>
-        <input type="radio" name ="q2" value ="notAtAllEasy"/>Not at all easy<br/><br/>
+        <input type="radio" name ="q2" value ="extremelyEasy" onChange ={this.handleQuestionChange}/>Extremely easy<br/>
+        <input type="radio" name ="q2" value ="veryEasy" onChange ={this.handleQuestionChange}/>Very easy<br/>
+        <input type="radio" name ="q2" value ="somewhatEasy" onChange ={this.handleQuestionChange}/>Somewhat easy<br/>
+        <input type="radio" name ="q2" value ="notSoEasy" onChange ={this.handleQuestionChange}/>Not so easy<br/>
+        <input type="radio" name ="q2" value ="notAtAllEasy" onChange ={this.handleQuestionChange}/>Not at all easy<br/><br/>
 
         <label>3.Did it take you more or less time than you expeted to find what you were looking for on our website</label><br/>
-        <input type="radio" name ="q3" value ="ALotLessTime"/>A lot less time<br/>
-        <input type="radio" name ="q3" value ="ALittleLessTime"/>A little less time<br/>
-        <input type="radio" name ="q3" value ="AboutWhatIExpected"/>About what I expected<br/>
-        <input type="radio" name ="q3" value ="ALittleMoreTime"/>A little more time<br/>
-        <input type="radio" name ="q3" value ="ALotMoreTime"/>A lot more time<br/><br/>
+        <input type="radio" name ="q3" value ="ALotLessTime" onChange ={this.handleQuestionChange}/>A lot less time<br/>
+        <input type="radio" name ="q3" value ="ALittleLessTime" onChange ={this.handleQuestionChange}/>A little less time<br/>
+        <input type="radio" name ="q3" value ="AboutWhatIExpected" onChange ={this.handleQuestionChange}/>About what I expected<br/>
+        <input type="radio" name ="q3" value ="ALittleMoreTime" onChange ={this.handleQuestionChange}/>A little more time<br/>
+        <input type="radio" name ="q3" value ="ALotMoreTime" onChange ={this.handleQuestionChange}/>A lot more time<br/><br/>
 
         <label>4.How visually applealling is our website?</label><br/>
-        <input type="radio" name ="q4" value ="extremelyApplealing"/>Extremely applealing<br/>
-        <input type="radio" name ="q4" value ="veryApplealing"/>Very applealing<br/>
-        <input type="radio" name ="q4" value ="somewhatApplealing"/>Somewhat applealing<br/>
-        <input type="radio" name ="q4" value ="notSoApplealing"/>Not so applealing<br/>
-        <input type="radio" name ="q4" value ="notAtAllApplealing"/>Not at all applealing<br/><br/>
+        <input type="radio" name ="q4" value ="extremelyApplealing" onChange ={this.handleQuestionChange}/>Extremely applealing<br/>
+        <input type="radio" name ="q4" value ="veryApplealing" onChange ={this.handleQuestionChange}/>Very applealing<br/>
+        <input type="radio" name ="q4" value ="somewhatApplealing" onChange ={this.handleQuestionChange}/>Somewhat applealing<br/>
+        <input type="radio" name ="q4" value ="notSoApplealing" onChange ={this.handleQuestionChange}/>Not so applealing<br/>
+        <input type="radio" name ="q4" value ="notAtAllApplealing onChange ={this.handleQuestionChange}"/>Not at all applealing<br/><br/>
 
         <label>5.How easy is it to understand the information on our website?</label><br/>
-        <input type="radio" name ="q5" value ="extremelyEasy"/>Extremely easy<br/>
-        <input type="radio" name ="q5" value ="veryEasy"/>Very easy<br/>
-        <input type="radio" name ="q5" value ="somewhatEasy"/>Somewhat easy<br/>
-        <input type="radio" name ="q5" value ="notSoEasy"/>Not so easy<br/>
-        <input type="radio" name ="q5" value ="notAtAllEasy"/>Not at all easy<br/><br/>
+        <input type="radio" name ="q5" value ="extremelyEasy" onChange ={this.handleQuestionChange}/>Extremely easy<br/>
+        <input type="radio" name ="q5" value ="veryEasy" onChange ={this.handleQuestionChange}/>Very easy<br/>
+        <input type="radio" name ="q5" value ="somewhatEasy" onChange ={this.handleQuestionChange}/>Somewhat easy<br/>
+        <input type="radio" name ="q5" value ="notSoEasy" onChange ={this.handleQuestionChange}/>Not so easy<br/>
+        <input type="radio" name ="q5" value ="notAtAllEasy" onChange ={this.handleQuestionChange}/>Not at all easy<br/><br/>
 
         <button className ="right" type="submit">Submit</button>
       </form>
